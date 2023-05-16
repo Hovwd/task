790ac752822bb7fe45e023e6b6c46e7a40d0e624
hovhannes <amirkhanyanhovhannes@gmail.com>
2023-05-17 02:33:35 +0400

    check warning

--- src/main/java/com/podcastle/task/dto/PhotoMetaDataWithStatisticsDTO.java
+++ src/main/java/com/podcastle/task/dto/PhotoMetaDataWithStatisticsDTO.java
@@ -1,8 +1,10 @@
 package com.podcastle.task.dto;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper=false)
 public class PhotoMetaDataWithStatisticsDTO extends PhotoMetaTataDTO{
     private StatisticDTO statistic;
 }
